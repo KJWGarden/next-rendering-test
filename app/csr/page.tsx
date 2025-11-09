8da4548623ecf2a6e186548b62a11dea46694cd3
KJWGarden <mike03131313@gmail.com>
2025-11-09 22:26:06 +0900

    feat: CSR 페이지에서 PostList 연동
    supabase에서 데이터 패칭 및 클라이언트 사이드에서 렌더링

--- app/csr/page.tsx
+++ app/csr/page.tsx
@@ -1,9 +1,35 @@
 "use client";
 
+import { supabase } from "@/lib/supabaseClient";
+import { useEffect, useState } from "react";
+import PostList from "../component/PostList";
+
+type Post = {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+};
+
 export default function CSRPage() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data, error } = await supabase.from("posts").select("*");
+      if (error) {
+        console.error("fething 하는중 오류 발생 :", error);
+      } else if (data) {
+        setPosts(data);
+      }
+    }
+
+    fetchPosts();
+  }, []);
   return (
     <main className="p-6">
       <h1 className="text-xl font-bold">CSR (Client-Side Rendering)</h1>
+      <PostList posts={posts} />
     </main>
   );
 }
