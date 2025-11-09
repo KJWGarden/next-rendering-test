41c0ee176cbc7e8a21e92c4de3207a3bb9e0ca66
KJWGarden <mike03131313@gmail.com>
2025-11-09 18:40:57 +0900

    feat:csr 페이지 추가
    Client Side Rendering 페이지 추가

---
+++ app/csr/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CSRPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-xl font-bold">CSR (Client-Side Rendering)</h1>
+    </main>
+  );
+}
