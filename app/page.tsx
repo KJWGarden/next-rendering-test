6255d8457bae6a3a3ab8a8ae7ff1f65fbacb7ad8
KJWGarden <mike03131313@gmail.com>
2025-11-19 16:25:28 +0900

    design: 홈페이지 UI CSS 수정
    UI 패딩 조절

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className="w-full h-full">
       <Navbar />
-      <RenderingMetricsChart />
+      <div className="container mx-auto px-4 py-8">
+        <RenderingMetricsChart />
+      </div>
     </main>
   );
 }
